d96f7988d7bb8da9463311605ebb1aa90a21e1f7
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-23 01:00:21 -0600

    Containerize app and change README.

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:12.16.3-stretch
+
+# Expose ports in the container
+EXPOSE 3000
+
+# Create service directory
+RUN mkdir -p /example
+WORKDIR /example
+
+# Install dependencies
+COPY package*.json ./
+RUN npm install --no-progress
+
+# Copy source code
+COPY . .
+
+# Set default command that is called when the container runs
+CMD npm run db:migrate && npm run start
--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,22 @@
 'use strict'
 
 // ref: https://devhints.io/knex
-// TODO: implement more dynamic env var settings loader
 module.exports = {
   development: {
-    client: 'pg',
-    connection: 'postgres://localhost:5432/knex-test',
+    client: 'postgresql',
+    connection: {
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+    },
     migrations: {
-      tableName: 'knex_migrations',
+      //tableName: 'knex_migrations',
       directory: `${ __dirname }/db/migrations`
     },
     seeds: {
       directory: `${ __dirname }/db/seeds`
-    }
-  },
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
     }
   }
 }
